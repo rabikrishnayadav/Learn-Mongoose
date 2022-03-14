6e7e337a8b22f7a651c8411d93ccfc7dbce36cd5
Rabi Kr Yadav <rabikrishnayadav@gmail.com>
2022-03-14 14:46:59 +0545

    schema and model

--- src/app.js
+++ src/app.js
@@ -8,3 +8,36 @@ mongoose.connect("mongodb://localhost:27017/leranmongoose",{
     useNewUrlParser: true, useUnifiedTopology:true
 }).then( () => console.log("Connection successful..."))
 .catch((err) => console.log(err));
+
+/*
+Schema :- 
+A Mongoose schema defines the structure of the document.
+Default values, validatiors etc.
+*/
+
+const  playlistSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    ctype: String,
+    videos: Number,
+    author:String,
+    active: Boolean,
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+
+/* 
+Model:-
+A Mongoose model is a wrapper on the the mongoose schema.
+A Mongoose schema defines the structure of the document,
+default values, validators, etc.
+where as a mongoose model provides an interface to the database for creating, querying, updating, deleting records, etc.
+*/
+
+// collection creation
+const Playlist = new mongoose.model("Playlist", playlistSchema);
